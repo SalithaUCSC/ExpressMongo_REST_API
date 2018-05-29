1fc36ca44fd7d0de14cc6cda39b030050c09b06e
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-05-30 03:44:36 +0530

    Add Api functionalities

--- api/routes/router.js
+++ api/routes/router.js
@@ -5,8 +5,69 @@ const router = express.Router();
 let Post = require('../models/post');
 
 router.get('/posts', function (req, res) {
-    res.send('posts page');
+    let posts = Post.find({}, function(err, posts){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.json(posts);
+        }
+    });
 });
 
+// Load single post view
+router.get('/posts/:id', function (req, res) {
+    Post.findById(req.params.id, function(err, post){
+        res.json(post);
+    });
+});
+
+// Add post
+router.post('/posts/add', function (req, res) {
+
+    let post = new Post();
+    post.title = req.body.title;
+    post.author = req.body.author;
+    post.description = req.body.description;
+    post.slug = req.body.slug;
+
+    post.save(function(err){
+        if(err){
+            console.log(err);
+            res.json({msg: "failed"})
+        }
+        else{
+            res.json({msg: "success"})
+        }
+    });
+
+
+});
+
+// Edit post
+router.get('/posts/edit/:id', function (req, res) {
+    Post.findById(req.params.id, function(err, post){
+        res.json(post);
+    });
+});
+// Update post
+router.post('/posts/update/:id', function (req, res) {
+    let post = {};
+    post.title = req.body.title;
+    post.author = req.body.author;
+    post.description = req.body.description;
+    
+    let query = { _id: req.params.id }
+
+    Post.update(query, post, function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+        else{
+            res.json({msg: "success"})
+        }
+    });
+});
 
 module.exports = router;
